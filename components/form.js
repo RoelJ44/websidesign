7a8ad2cb3c06d53ae913a8322f937efb5f3f3361
RoelJ44 <roeljanssen2002@gmail.com>
2022-06-08 16:19:47 +0200

    main

--- components/form.js
+++ components/form.js
@@ -38,7 +38,6 @@ export default function Form() {
     const name = event.target.name;
     const value = event.target.value;
     setInputs((values) => ({ ...values, [name]: value }));
-    setState({ ...state, [event.target.name]: event.target.value });
   };
 
   // Single inputs checks
