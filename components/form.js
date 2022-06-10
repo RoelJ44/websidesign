653cec787edf7bce9eef8bdde7a05e01dc4173ab
RoelJ44 <roeljanssen2002@gmail.com>
2022-06-10 12:04:21 +0200

    main

--- components/form.js
+++ components/form.js
@@ -110,23 +110,6 @@ export default function Form() {
     }
   };
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   fetch("/", {
-  //     method: "POST",
-  //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //     body: encode({
-  //       "form-name": "testForm",
-  //       ...inputs,
-  //     }),
-  //   })
-  //     .then(function () {
-  //       setLoader(false);
-  //       setStatus("Verzonden");
-  //     })
-  //     .catch((error) => alert(error));
-  // };
-
   const handleSubmit = (event) => {
     fetch("/", {
       method: "POST",
@@ -166,19 +149,6 @@ export default function Form() {
           <span className="ml-1">Aan het laden...</span>
         </div>
       )}
-      {/* <form name="contact-form" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact-form" />
-
-        <label htmlFor="name">Name *</label>
-        <input id="name" name="name" required type="text" />
-        <label htmlFor="company">Company *</label>
-        <input id="company" name="company" required type="text" />
-        <label htmlFor="email">E-mail Address *</label>
-        <input id="email" type="email" name="email" required />
-        <label htmlFor="message">Message *</label>
-        <textarea id="message" name="message" required></textarea>
-        <button type="submit">Submit</button>
-      </form> */}
       <form name="contact-form" method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="contact-form" />
         <div className="w-full md:top-20 max-w-md mx-auto sm:max-w-none">
@@ -284,11 +254,7 @@ export default function Form() {
                 setLoader(false);
                 setStatus("");
               }}
-              // onClick={() => {
-              //   handleForm(event);
-              // }}
               type="button"
-              // type="submit"
               className="relative bg-blue text-white w-auto px-6 py-2.5 font-swiss font-bold"
             >
               Verzenden
